4636d4d8b6372c1ba9fba0d090c331cd595ce2f6
riley <riley@nixos>
2026-06-28 14:31:28 -0500

    initial flake setup

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "riley's nixos config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
